abc1c728f3f91ac18bae31bee5d0276def760f6e
Dylan <portaldylan12@gmail.com>
2024-06-03 19:48:40 -0300

    punto 3 terminado

--- Practica6-parte1/punto3/Punto3.pas
+++ Practica6-parte1/punto3/Punto3.pas
@@ -1,11 +1,48 @@
-program Punto3;
-
+program JugamosConListas;
 type
-  {Tipos}
+    lista = ^nodo;
+    nodo = record
+        num : integer;
+        sig : lista;
+    end;
+
+procedure armarNodo(var L: lista; v: integer;var ult:lista);
+var
+    aux : lista;
+begin
+    new(aux);
+    aux^.num:=v;
+    aux^.sig:=nil;
+    
+    if (L=nil) then
+        L:=aux
+    else
+        ult^.sig:=aux;
+    ult:=aux;
+
+end;
+
+procedure imprimirNumero(pri : lista);
+begin
+    while(pri <> nil) do
+    begin
+        writeln(pri^.num);
+        pri := pri^.sig;
+    end;
+end;
 
 var
-  {Variables}
-
+    pri,ult : lista;
+    valor : integer;
 begin
-  {Programa principal}
+    pri := nil;
+    write('ingrese un numero: ');
+    readln(valor);
+    while (valor <> 0) do 
+    begin
+        armarNodo(pri, valor,ult);
+        write('ingrese un numero: ');
+        readln(valor);
+    end;
+    imprimirNumero(pri);
 end.
